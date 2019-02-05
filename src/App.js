31732682c1121cf5b3eb16c1ccf9d306f86c6404
Jmanblog <jmanblog@protonmail.com>
2019-02-05 13:59:59 +0200

    Added route and custom api call to ArticleApp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    console.log(this.props.location);
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            This is my own text from <code>src/App.js</code>.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <h1>Frontpage</h1>
         </header>
       </div>
     );
---
+++ src/ArticleApp.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+class Article extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = { loaded: false, data: null};
+
+        console.log("Article constructor complete.");
+    }
+    
+    componentDidMount() {
+        console.log("The component was mounted.");
+        let componentState = this;
+        let componentUrl = this.props.location.pathname;
+        console.log("Props.location=");
+        console.log(this.props.location);
+        console.log("Current url= " + componentUrl);
+        let fetchUrl = "https://localhost:44362/api" + componentUrl;
+        fetch(fetchUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            console.log("JSON-data on ladattu!");
+            console.log(JSON.stringify(myJson));
+
+            console.log("Kutsutaan setState()-rutiinia.");
+            componentState.setState({ loaded: true, data: myJson });
+            console.log("setState()-rutiinia on kutsuttu.");
+        });
+    }
+  
+    render() {
+        console.log("Aloitetaan render()");
+        if (this.state.loaded === false) {
+            return(
+                <div>Ladataan...</div>
+            )
+        }
+        else {
+            let myData = this.state.data;
+
+        console.log("Data luettu, kirjoitetaan.");
+
+            return (
+                <div className="Article" id="Article">
+                <header className="App-header">
+                <h1>{myData[0].articleTitle}</h1>
+                <img src={myData[0].articleImage} alt={myData[0].articleTitle} height="200px"/>
+                <div>{myData[0].articleContent}</div>
+                </header>
+                </div>
+            );
+        }
+    }
+}
+
+export default Article;
